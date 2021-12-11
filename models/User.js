f101f39a122c027b309e28bb5fdfe59d36812cb1
Sophia Barrett <sophia@yoursummit.media>
2021-12-10 19:02:26 -0700

    initial user model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { timeStamp } = require('console');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: { len: [6] }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
