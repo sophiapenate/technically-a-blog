88e98bc345b3b781a17665801a057c52aa4cd104
Sophia Barrett <sophia@yoursummit.media>
2021-12-11 16:25:53 -0700

    setup bootstrap

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,33 +1,32 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-    Post.findAll({
+  Post.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
         include: [
-            {
-                model: User,
-                attributes: ['username']
-            },
-            {
-                model: Comment,
-                include: [
-                    {
-                        model: User,
-                        attributes: ['username']
-                    }
-                ]
-            }
-        ]
+          {
+            model: User,
+            attributes: ["username"],
+          },
+        ],
+      },
+    ],
+  })
+    .then((dbData) => {
+      const posts = dbData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
     })
-    .then(dbData => {
-        const posts = dbData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });    
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
-
 
 module.exports = router;
