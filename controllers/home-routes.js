d1ee33b3cab44d41bbaf419ad7c55a0d4f0850ad
Sophia Barrett <sophia@yoursummit.media>
2021-12-12 12:26:47 -0700

    setup login view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { render } = require("express/lib/response");
 const { User, Post, Comment } = require("../models");
 
 router.get("/", (req, res) => {
@@ -27,6 +28,14 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;
