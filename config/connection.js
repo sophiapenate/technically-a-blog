514268891f0b1d8e18421147b9e25cc3f8caae4a
Sophia Barrett <sophia@yoursummit.media>
2021-12-13 14:01:26 -0700

    config for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,24 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 //import env variables
-require('dotenv').config();
+require("dotenv").config();
 
-// create db connection
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-})
+// check if running remotely with JAWSDB
+if (process.env.JAWSDB_URL) {
+  // create remote db connection
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // create local db connection
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
