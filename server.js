e9be244b28f45b0ec4c5929a03b8283edd4c23d9
Sophia Barrett <sophia@yoursummit.media>
2021-12-12 17:16:02 -0700

    create dashboard view

--- server.js
+++ server.js
@@ -31,11 +31,6 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// bootstrap
-// app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-// app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-//app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-
 // set routes
 app.use(routes);
 
