20bd1a9590072359d343b67d2f88f9d2303c46cd
Sophia Barrett <sophia@yoursummit.media>
2021-12-13 16:52:54 -0700

    auto add current year to footer

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,23 +1,24 @@
-function format_date(date) {
+module.exports = {
+  format_date: (date) => {
     const options = {
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric'
-    }
-    return new Date(date).toLocaleString('en-US', options);
-};
-
-function format_plural(word, count) {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    };
+    return new Date(date).toLocaleString("en-US", options);
+  },
+  format_plural: (word, count) => {
     if (count !== 1) {
-        return `${word}s`;
+      return `${word}s`;
     }
     return word;
-};
-
-function format_capital(name) {
-    const lettersArr = name.split('');
+  },
+  format_capital: (name) => {
+    const lettersArr = name.split("");
     lettersArr[0] = lettersArr[0].toUpperCase();
-    return lettersArr.join('');
-}
-
-module.exports = { format_date, format_plural, format_capital }
+    return lettersArr.join("");
+  },
+  current_year: () => {
+    return new Date().getFullYear();
+  },
+};
